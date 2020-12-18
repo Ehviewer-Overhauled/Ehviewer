5b1742ba9d9ea636f680a5b4607b6c1a47d6b8f1
NekoInverter <7741672-NekoInverter@users.noreply.gitlab.com>
2020-12-18 19:13:52 +0800

    Fix snackbar on pre-29

--- app/src/main/java/com/hippo/ehviewer/ui/MainActivity.java
+++ app/src/main/java/com/hippo/ehviewer/ui/MainActivity.java
@@ -25,6 +25,7 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.text.TextUtils;
@@ -318,7 +319,7 @@ public final class MainActivity extends StageActivity
             ((EhApplication) getApplication()).recreate();
             Settings.putTheme(theme);
         });
-        mDrawerLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) mDrawerLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         updateProfile();
