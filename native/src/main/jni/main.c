7307c136ee5d492ea6e5d51e29f9f3d0a1bd27c1
tarsin <yuanqingxiang233@163.com>
2022-05-21 17:24:03 +0800

    Drop useless native part

--- native/src/main/jni/main.c
+++ native/src/main/jni/main.c
@@ -1,11 +1,11 @@
 #include <jni.h>
 
-#include "genuine.h"
 #include "java_wrapper.h"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     JNIEnv* env;
+
     if ((*vm)->GetEnv(vm, (void**) (&env), JNI_VERSION_1_6) != JNI_OK) {
         return JNI_ERR;
     }
