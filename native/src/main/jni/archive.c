ef2441116db35846c8cb29f77c104b3a66fc3395
tarsin <yuanqingxiang233@163.com>
2022-05-22 23:17:35 +0800

    Use more friendly suffix recogniser and support all image file

--- native/src/main/jni/archive.c
+++ native/src/main/jni/archive.c
@@ -11,10 +11,14 @@
 #include <archive_entry.h>
 
 #define LOG_TAG "libarchive"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG ,__VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG ,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG ,__VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG ,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG ,__VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG ,__VA_ARGS__)
 
-#define MAX_ENTRY_LIMIT 4096
-#define BLOCK_SIZE 10240
+#define BLOCK_SIZE 4096
 
 #define READ_DATA_ERR 3
 
@@ -31,16 +35,35 @@ typedef struct archive_inst_sc {
     jbyteArray jbr;
 } archive_inst;
 
+static int filename_is_playable_file(const char* name)
+{
+    const char* dotptr = strrchr(name, '.');
+    if (dotptr++) {
+        switch (*dotptr) {
+            case 'b':
+                return strcmp(dotptr, "bmp") == 0;
+            case 'j':
+                return (strcmp(dotptr, "jpg") == 0) || (strcmp(dotptr, "jpeg") == 0);
+            case 'p':
+                return strcmp(dotptr, "png") == 0;
+            case 'g':
+                return strcmp(dotptr, "gif") == 0;
+            case 'w':
+                return strcmp(dotptr, "webp") == 0;
+            default:
+                return 0;
+        }
+    }
+    return 0;
+}
+
 static void archive_list_all_entries(struct archive_inst_sc *inst)
 {
     struct archive *a = inst->archive;
     struct archive_entry *entry;
     int count = 0;
     while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-        const char* name = archive_entry_pathname(entry);
-        const char* suffix = name + strlen(name) - 4;
-        LOGI("%s", name);
-        if (strcmp(suffix, ".jpg") == 0)
+        if(filename_is_playable_file(archive_entry_pathname(entry)))
             count++;
     }
     inst->entry_num = count;
@@ -113,17 +136,13 @@ Java_com_hippo_UriArchiveAccessor_extracttoOutputStream(JNIEnv *env, jobject thi
     int32_t size;
     jmethodID writeID = (*env)->GetMethodID(env, (*env)->GetObjectClass(env, os), "write", "([B)V");
     jmethodID flushID = (*env)->GetMethodID(env, (*env)->GetObjectClass(env, os), "flush", "()V");
-    LOGI("%s%d", "Start extract index ", index);
     archive_read_open(arc->archive, arc, NULL, ins_read, ins_close);
     while (archive_read_next_header(arc->archive, &entry) == ARCHIVE_OK) {
-        const char* name = archive_entry_pathname(entry);
-        const char* suffix = name + strlen(name) - 4;
-        if (strcmp(suffix, ".jpg") != 0)
+        if(!filename_is_playable_file(archive_entry_pathname(entry)))
             continue;
         if (count == index) {
             for (;;) {
-                size = archive_read_data(arc->archive, arc->output_buffer, BLOCK_SIZE);
-                LOGI("%s%d%s%d", "index ", index, "size ", size);
+                size = (int32_t)archive_read_data(arc->archive, arc->output_buffer, BLOCK_SIZE);
                 if (size < 0) {
                     return READ_DATA_ERR;
                 }
@@ -137,7 +156,6 @@ Java_com_hippo_UriArchiveAccessor_extracttoOutputStream(JNIEnv *env, jobject thi
         }
         count++;
     }
-    LOGI("%s%d", "Stop extract index ", index);
     archive_destroy_inst(arc);
     return 0;
 }
