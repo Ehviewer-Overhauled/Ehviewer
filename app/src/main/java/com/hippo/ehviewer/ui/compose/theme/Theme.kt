b1a431ed828ba4d6ab1d43bb4258885202d9dbd9
tarsin <yuanqingxiang233@163.com>
2023-01-10 18:17:10 +0800

    Use mca component to judge is dynamic color available

--- app/src/main/java/com/hippo/ehviewer/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/compose/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.hippo.ehviewer.ui.compose.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,6 +8,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.google.android.material.color.DynamicColors
 
 private val DarkColorScheme = darkColorScheme()
 private val LightColorScheme = lightColorScheme()
@@ -16,12 +16,10 @@ private val LightColorScheme = lightColorScheme()
 @Composable
 fun EhViewerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        DynamicColors.isDynamicColorAvailable() -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
