5910728ca65f4735f3552f41d53209d40314a1cd
小白-白 <i@moedog.org>
2023-01-15 13:57:24 +0800

    Fix wrong extension when downloading

--- app/src/main/java/com/hippo/yorozuya/Utilities.java
+++ app/src/main/java/com/hippo/yorozuya/Utilities.java
@@ -16,21 +16,9 @@
 
 package com.hippo.yorozuya;
 
-import android.text.TextUtils;
-import android.webkit.MimeTypeMap;
-
 import androidx.annotation.Nullable;
 
-import java.util.regex.Pattern;
-
 public final class Utilities {
-
-    private static final String[] MIME_TYPE_ARRAY = {
-            "image/jpg"
-    };
-    private static final String[] EXTENSION_ARRAY = {
-            "jpg"
-    };
 
     /**
      * Whether the array contain the element
@@ -70,49 +58,5 @@ public final class Utilities {
             }
         }
         return false;
-    }
-
-    public static String getNameFromUrl(String url) {
-        if (!TextUtils.isEmpty(url)) {
-            int fragment = url.lastIndexOf('#');
-            if (fragment > 0) {
-                url = url.substring(0, fragment);
-            }
-
-            int query = url.lastIndexOf('?');
-            if (query > 0) {
-                url = url.substring(0, query);
-            }
-
-            int filenamePos = url.lastIndexOf('/');
-            String filename =
-                    0 <= filenamePos ? url.substring(filenamePos + 1) : url;
-
-            // if the filename contains special characters, we don't
-            // consider it valid for our matching purposes:
-            if (filename.length() != 0 &&
-                    Pattern.matches("[a-zA-Z_0-9\\.\\-\\(\\)%]+", filename)) {
-                int dotPos = filename.lastIndexOf('.');
-                if (0 < dotPos) {
-                    return filename.substring(0, dotPos);
-                }
-            }
-        }
-
-        return null;
-    }
-
-    public static String getExtensionFromMimeType(String mimeType) {
-        if (TextUtils.isEmpty(mimeType)) {
-            return null;
-        }
-
-        for (int i = 0, n = MIME_TYPE_ARRAY.length; i < n; i++) {
-            if (MIME_TYPE_ARRAY[i].equals(mimeType)) {
-                return EXTENSION_ARRAY[i];
-            }
-        }
-
-        return MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType);
     }
 }
