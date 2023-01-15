ce274cf65b5dca1dcc7ddaa980ac71110b2b9f74
小白-白 <i@moedog.org>
2023-01-15 13:56:52 +0800

    Hide unavailable archive

--- app/src/main/java/com/hippo/ehviewer/client/parser/ArchiveParser.java
+++ app/src/main/java/com/hippo/ehviewer/client/parser/ArchiveParser.java
@@ -49,12 +49,15 @@ public class ArchiveParser {
         var es = d.select("#db>div>div");
         for (var e : es) {
             try {
-                var res = e.selectFirst("form>input").attr("value");
-                var name = e.selectFirst("form>div>input").attr("value");
-                var size = e.selectFirst("p>strong").text();
-                var cost = e.selectFirst("div>strong").text();
-                var item = new Archive(res, name, size, cost, false);
-                archiveList.add(item);
+                var btn = e.selectFirst("form>div>input");
+                if (!btn.hasAttr("disabled")) {
+                    var res = e.selectFirst("form>input").attr("value");
+                    var name = btn.attr("value");
+                    var size = e.selectFirst("p>strong").text();
+                    var cost = e.selectFirst("div>strong").text().replace(",", "");
+                    var item = new Archive(res, name, size, cost, false);
+                    archiveList.add(item);
+                }
             } catch (NullPointerException ex) {
                 ex.printStackTrace();
             }
