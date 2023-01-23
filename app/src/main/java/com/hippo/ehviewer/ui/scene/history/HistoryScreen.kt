61c75c06a9f8555445dcd97c34fb6bd10bd649c4
tarsin <yuanqingxiang233@163.com>
2023-01-23 10:36:44 +0800

    History Screen no item tips

--- app/src/main/java/com/hippo/ehviewer/ui/scene/history/HistoryScreen.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/history/HistoryScreen.kt
@@ -1,5 +1,9 @@
 package com.hippo.ehviewer.ui.scene.history
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -7,15 +11,18 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
@@ -69,5 +76,28 @@ fun HistoryScreen(toggleDrawer: () -> Unit) {
                 }
             }
         }
+
+        if (historyData.itemCount == 0) {
+            NoHistory()
+        }
+    }
+}
+
+@Composable
+private fun NoHistory() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.big_history),
+            contentDescription = null,
+            Modifier.padding(16.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.no_history),
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
