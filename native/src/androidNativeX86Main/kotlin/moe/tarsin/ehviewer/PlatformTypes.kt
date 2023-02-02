7492841b5f7a11e7b0db293ca6a622ed29cc6e16
tarsin <yuanqingxiang233@163.com>
2023-02-02 19:47:16 +0800

    Check Long to size_t conversion

--- native/src/androidNativeX86Main/kotlin/moe/tarsin/ehviewer/PlatformTypes.kt
+++ native/src/androidNativeX86Main/kotlin/moe/tarsin/ehviewer/PlatformTypes.kt
@@ -2,6 +2,9 @@ package moe.tarsin.ehviewer
 
 import platform.posix.size_t
 
+private val MAX_ALLOWED_CONVERT_VALUE = size_t.MAX_VALUE.toLong()
+
 actual fun Long.tosize_t(): size_t {
+    check(this <= MAX_ALLOWED_CONVERT_VALUE)
     return toUInt()
 }
