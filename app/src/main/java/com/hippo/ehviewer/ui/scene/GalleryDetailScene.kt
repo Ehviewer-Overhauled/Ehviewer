01d8dabef4d52fe0978dc9d16cc2c8e4d0e01bc5
tarsin <yuanqingxiang233@163.com>
2023-02-04 09:47:16 +0800

    Resolve #756

--- app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryDetailScene.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryDetailScene.kt
@@ -573,7 +573,7 @@ class GalleryDetailScene : CollapsingToolbarScene(), View.OnClickListener, Downl
             uploader.text = gd.uploader
             uploader.alpha = if (gd.disowned) .5f else 1f
             category.text = EhUtils.getCategory(gd.category)
-            category.setTextColor(EhUtils.getCategoryColor(gd.category))
+            EhUtils.getCategoryColorNullable(gd.category)?.let { category.setTextColor(it) }
         }
         updateDownloadText()
         binding.content.header.run {
