61fd8dc778838c8cd55f0e79e595f9d7e7607986
tarsin <yuanqingxiang233@163.com>
2023-02-18 12:55:59 +0800

    Fix transfer failed msg

--- app/src/main/java/com/hippo/yorozuya/IOUtils.kt
+++ app/src/main/java/com/hippo/yorozuya/IOUtils.kt
@@ -4,11 +4,13 @@ import okhttp3.ResponseBody
 import java.io.File
 
 fun ResponseBody.copyToFile(file: File) {
+    val length = contentLength()
+    check(length > 0) { "contentLength < 0!" }
     val os = file.outputStream()
     val channel = os.channel
     val source = source()
     try {
-        channel.transferFrom(source, 0, contentLength())
+        channel.transferFrom(source, 0, length)
     } catch (e: Throwable) {
         throw e
     } finally {
