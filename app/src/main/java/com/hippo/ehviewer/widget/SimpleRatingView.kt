83dc47c3b53fefbbebcb91a5cc366741060be032
tarsin <yuanqingxiang233@163.com>
2023-02-25 23:40:01 +0800

    Fix SimpleRatingWidget state changed recompose and properly set theme

--- app/src/main/java/com/hippo/ehviewer/widget/SimpleRatingView.kt
+++ app/src/main/java/com/hippo/ehviewer/widget/SimpleRatingView.kt
@@ -3,7 +3,11 @@ package com.hippo.ehviewer.widget
 import android.content.Context
 import android.util.AttributeSet
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.AbstractComposeView
+import com.google.accompanist.themeadapter.material3.Mdc3Theme
 import com.hippo.ehviewer.ui.widget.SimpleRatingWidget
 
 class SimpleRatingView @JvmOverloads constructor(
@@ -11,10 +15,12 @@ class SimpleRatingView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
 ) : AbstractComposeView(context, attrs, defStyle) {
-    var rating: Float = 0.0f
+    var rating by mutableStateOf(0f)
 
     @Composable
     override fun Content() {
-        SimpleRatingWidget(rating)
+        Mdc3Theme {
+            SimpleRatingWidget(rating)
+        }
     }
 }
