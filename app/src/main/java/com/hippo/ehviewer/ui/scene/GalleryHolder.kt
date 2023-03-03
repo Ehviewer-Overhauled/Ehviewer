e9839335c6a4e5109cf98fcb67e0d1567c3f8637
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-03-03 20:11:19 +0800

    Convert GalleryHolder to view binding

--- app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryHolder.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryHolder.kt
@@ -15,46 +15,88 @@
  */
 package com.hippo.ehviewer.ui.scene
 
+import android.annotation.SuppressLint
+import android.text.TextUtils
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
-import com.hippo.ehviewer.R
-import com.hippo.ehviewer.widget.SimpleRatingView
-import com.hippo.widget.LoadImageView
+import androidx.viewbinding.ViewBinding
+import com.hippo.drawable.TriangleDrawable
+import com.hippo.ehviewer.Settings
+import com.hippo.ehviewer.client.EhUtils
+import com.hippo.ehviewer.client.data.GalleryInfo
+import com.hippo.ehviewer.databinding.ItemGalleryGridBinding
+import com.hippo.ehviewer.databinding.ItemGalleryListBinding
+import com.hippo.ehviewer.download.DownloadManager
 
-internal class GalleryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    @JvmField
-    val thumb: LoadImageView = itemView.findViewById(R.id.thumb)
+internal abstract class GalleryHolder(binding: ViewBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    abstract fun bind(galleryInfo: GalleryInfo)
+}
 
-    @JvmField
-    val title: TextView? = itemView.findViewById(R.id.title)
+internal class ListGalleryHolder(
+    private val binding: ItemGalleryListBinding,
+    private val showFavourited: Boolean,
+    thumbWidth: Int,
+    thumbHeight: Int
+) : GalleryHolder(binding) {
+    init {
+        val lp = binding.thumb.layoutParams
+        lp.width = thumbWidth
+        lp.height = thumbHeight
+        binding.thumb.layoutParams = lp
+    }
 
-    @JvmField
-    val uploader: TextView? = itemView.findViewById(R.id.uploader)
+    @SuppressLint("SetTextI18n")
+    override fun bind(galleryInfo: GalleryInfo) {
+        binding.run {
+            thumb.load(galleryInfo.thumb!!)
+            title.text = EhUtils.getSuitableTitle(galleryInfo)
+            uploader.text = galleryInfo.uploader
+            uploader.alpha = if (galleryInfo.disowned) .5f else 1f
+            rating.rating = galleryInfo.rating
+            val newCategoryText = EhUtils.getCategory(galleryInfo.category)
+            if (newCategoryText != category.text.toString()) {
+                category.text = newCategoryText
+                category.setBackgroundColor(EhUtils.getCategoryColor(galleryInfo.category))
+            }
+            posted.text = galleryInfo.posted
+            if (galleryInfo.pages == 0 || !Settings.showGalleryPages) {
+                pages.text = null
+                pages.visibility = View.GONE
+            } else {
+                pages.text = "${galleryInfo.pages}P"
+                pages.visibility = View.VISIBLE
+            }
+            if (TextUtils.isEmpty(galleryInfo.simpleLanguage)) {
+                simpleLanguage.text = null
+                simpleLanguage.visibility = View.GONE
+            } else {
+                simpleLanguage.text = galleryInfo.simpleLanguage
+                simpleLanguage.visibility = View.VISIBLE
+            }
+            favourited.visibility =
+                if (showFavourited && galleryInfo.favoriteSlot >= -1 && galleryInfo.favoriteSlot <= 10) View.VISIBLE else View.GONE
+            downloaded.visibility =
+                if (DownloadManager.containDownloadInfo(galleryInfo.gid)) View.VISIBLE else View.GONE
+        }
+    }
+}
 
-    @JvmField
-    val rating: SimpleRatingView? = itemView.findViewById(R.id.rating)
-
-    @JvmField
-    val category: TextView = itemView.findViewById(R.id.category)
-
-    @JvmField
-    val posted: TextView? = itemView.findViewById(R.id.posted)
-
-    @JvmField
-    val pages: TextView? = itemView.findViewById(R.id.pages)
-
-    @JvmField
-    val simpleLanguage: TextView = itemView.findViewById(R.id.simple_language)
-
-    @JvmField
-    val favourited: ImageView? = itemView.findViewById(R.id.favourited)
-
-    @JvmField
-    val downloaded: ImageView? = itemView.findViewById(R.id.downloaded)
-
-    @JvmField
-    val card: MaterialCardView = itemView.findViewById(R.id.card)
+internal class GridGalleryHolder(private val binding: ItemGalleryGridBinding) :
+    GalleryHolder(binding) {
+    override fun bind(galleryInfo: GalleryInfo) {
+        binding.run {
+            thumb.setThumbSize(galleryInfo.thumbWidth, galleryInfo.thumbHeight)
+            thumb.load(galleryInfo.thumb!!)
+            var drawable = category.background
+            val color = EhUtils.getCategoryColor(galleryInfo.category)
+            if (drawable !is TriangleDrawable) {
+                drawable = TriangleDrawable(color)
+                category.background = drawable
+            } else {
+                drawable.setColor(color)
+            }
+            simpleLanguage.text = galleryInfo.simpleLanguage
+        }
+    }
 }
