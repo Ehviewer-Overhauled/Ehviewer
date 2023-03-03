9cf7231e78951f990061811da10ca17a7330e55a
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-03-03 20:11:19 +0800

    Convert GalleryHolder to view binding

--- app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryAdapter.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/GalleryAdapter.kt
@@ -17,23 +17,20 @@ package com.hippo.ehviewer.ui.scene
 
 import android.annotation.SuppressLint
 import android.content.res.Resources
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.IntDef
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.hippo.drawable.TriangleDrawable
 import com.hippo.easyrecyclerview.MarginItemDecoration
-import com.hippo.ehviewer.download.DownloadManager as downloadManager
 import com.hippo.ehviewer.R
 import com.hippo.ehviewer.Settings
 import com.hippo.ehviewer.client.EhUtils
 import com.hippo.ehviewer.client.data.GalleryInfo
 import com.hippo.ehviewer.dao.LocalFavoriteInfo
-import com.hippo.ehviewer.download.DownloadManager
-import com.hippo.ehviewer.widget.TileThumb
+import com.hippo.ehviewer.databinding.ItemGalleryGridBinding
+import com.hippo.ehviewer.databinding.ItemGalleryListBinding
 import com.hippo.widget.recyclerview.AutoStaggeredGridLayoutManager
 import com.hippo.yorozuya.ViewUtils
 
@@ -48,7 +45,6 @@ internal abstract class GalleryAdapter(
     private val mListThumbWidth: Int
     private val mListThumbHeight: Int
     private val mShowFavourited: Boolean
-    private val mDownloadManager: DownloadManager
     private var mListDecoration: MarginItemDecoration? = null
     private var mGirdDecoration: MarginItemDecoration? = null
     private var mType = TYPE_INVALID
@@ -63,8 +59,7 @@ internal abstract class GalleryAdapter(
             val recyclerView = mRecyclerView
             when (type) {
                 TYPE_LIST -> {
-                    val columnWidth =
-                        mResources.getDimensionPixelOffset(Settings.detailSizeResId)
+                    val columnWidth = mResources.getDimensionPixelOffset(Settings.detailSizeResId)
                     mLayoutManager.setColumnSize(columnWidth)
                     mLayoutManager.setStrategy(AutoStaggeredGridLayoutManager.STRATEGY_MIN_SIZE)
                     if (null != mGirdDecoration) {
@@ -119,7 +114,6 @@ internal abstract class GalleryAdapter(
         mListThumbHeight = calculator.measuredHeight
         mListThumbWidth = mListThumbHeight * 2 / 3
         this.type = type
-        mDownloadManager = downloadManager
     }
 
     private fun adjustPaddings() {
@@ -131,29 +125,22 @@ internal abstract class GalleryAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryHolder {
-        val layoutId = when (viewType) {
-            TYPE_LIST -> R.layout.item_gallery_list
-            TYPE_GRID -> R.layout.item_gallery_grid
+        val holder = when (viewType) {
+            TYPE_LIST -> ListGalleryHolder(
+                ItemGalleryListBinding.inflate(mInflater, parent, false),
+                mShowFavourited,
+                mListThumbWidth,
+                mListThumbHeight
+            )
+
+            TYPE_GRID -> GridGalleryHolder(ItemGalleryGridBinding.inflate(mInflater, parent, false))
             else -> throw IllegalStateException("Unexpected value: $viewType")
         }
-        val holder = GalleryHolder(mInflater.inflate(layoutId, parent, false))
-        if (viewType == TYPE_LIST) {
-            val lp = holder.thumb.layoutParams
-            lp.width = mListThumbWidth
-            lp.height = mListThumbHeight
-            holder.thumb.layoutParams = lp
+        holder.itemView.setOnClickListener {
+            onItemClick(holder.itemView, holder.bindingAdapterPosition)
         }
-        holder.card.setOnClickListener {
-            onItemClick(
-                holder.itemView,
-                holder.bindingAdapterPosition
-            )
-        }
-        holder.card.setOnLongClickListener {
-            onItemLongClick(
-                holder.itemView,
-                holder.bindingAdapterPosition
-            )
+        holder.itemView.setOnLongClickListener {
+            onItemLongClick(holder.itemView, holder.bindingAdapterPosition)
         }
         return holder
     }
@@ -166,61 +153,12 @@ internal abstract class GalleryAdapter(
 
     abstract fun getDataAt(position: Int): GalleryInfo?
 
-    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: GalleryHolder, position: Int) {
         val gi = getDataAt(position) ?: return
         if (gi is LocalFavoriteInfo) {
             gi.thumb = EhUtils.fixThumbUrl(gi.thumb!!)
         }
-        when (mType) {
-            TYPE_LIST -> {
-                holder.thumb.load(gi.thumb!!)
-                holder.title!!.text = EhUtils.getSuitableTitle(gi)
-                holder.uploader!!.text = gi.uploader
-                holder.uploader.alpha = if (gi.disowned) .5f else 1f
-                holder.rating!!.rating = gi.rating
-                val category = holder.category
-                val newCategoryText = EhUtils.getCategory(gi.category)
-                if (newCategoryText != category.text.toString()) {
-                    category.text = newCategoryText
-                    category.setBackgroundColor(EhUtils.getCategoryColor(gi.category))
-                }
-                holder.posted!!.text = gi.posted
-                if (gi.pages == 0 || !Settings.showGalleryPages) {
-                    holder.pages?.text = null
-                    holder.pages!!.visibility = View.GONE
-                } else {
-                    holder.pages!!.text = gi.pages.toString() + "P"
-                    holder.pages.visibility = View.VISIBLE
-                }
-                if (TextUtils.isEmpty(gi.simpleLanguage)) {
-                    holder.simpleLanguage.text = null
-                    holder.simpleLanguage.visibility = View.GONE
-                } else {
-                    holder.simpleLanguage.text = gi.simpleLanguage
-                    holder.simpleLanguage.visibility = View.VISIBLE
-                }
-                holder.favourited!!.visibility =
-                    if (mShowFavourited && gi.favoriteSlot >= -1 && gi.favoriteSlot <= 10) View.VISIBLE else View.GONE
-                holder.downloaded!!.visibility =
-                    if (mDownloadManager.containDownloadInfo(gi.gid)) View.VISIBLE else View.GONE
-            }
-
-            TYPE_GRID -> {
-                (holder.thumb as TileThumb).setThumbSize(gi.thumbWidth, gi.thumbHeight)
-                holder.thumb.load(gi.thumb!!)
-                val category: View = holder.category
-                var drawable = category.background
-                val color = EhUtils.getCategoryColor(gi.category)
-                if (drawable !is TriangleDrawable) {
-                    drawable = TriangleDrawable(color)
-                    category.background = drawable
-                } else {
-                    drawable.setColor(color)
-                }
-                holder.simpleLanguage.text = gi.simpleLanguage
-            }
-        }
+        holder.bind(gi)
     }
 
     @IntDef(TYPE_LIST, TYPE_GRID)
