a246fab610ed961c71a874093f7f062d268f3d92
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-04-02 15:17:25 +0800

    Convert GalleryNotAvailableParser to kotlin

--- app/src/main/java/com/hippo/ehviewer/client/parser/GalleryNotAvailableParser.kt
+++ app/src/main/java/com/hippo/ehviewer/client/parser/GalleryNotAvailableParser.kt
@@ -15,30 +15,22 @@
  * You should have received a copy of the GNU General Public License along with EhViewer.
  * If not, see <https://www.gnu.org/licenses/>.
  */
+package com.hippo.ehviewer.client.parser
 
-package com.hippo.ehviewer.client.parser;
+import com.hippo.util.ExceptionUtils
+import com.hippo.util.JsoupUtils
+import org.jsoup.Jsoup
 
-import com.hippo.util.ExceptionUtils;
-import com.hippo.util.JsoupUtils;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-public class GalleryNotAvailableParser {
-
-    public static String parse(String body) {
-        String error = null;
-        try {
-            Document document = Jsoup.parse(body);
-            Element d = JsoupUtils.getElementByClass(document, "d");
-            //noinspection ConstantConditions
-            error = d.child(0).html();
-            error = error.replace("<br>", "\n");
-        } catch (Throwable e) {
-            ExceptionUtils.throwIfFatal(e);
-            e.printStackTrace();
+object GalleryNotAvailableParser {
+    fun parse(body: String): String? {
+        return runCatching {
+            val document = Jsoup.parse(body)
+            val d = JsoupUtils.getElementByClass(document, "d")
+            d!!.child(0).html().replace("<br>", "\n")
+        }.getOrElse {
+            ExceptionUtils.throwIfFatal(it)
+            it.printStackTrace()
+            null
         }
-        return error;
     }
 }
