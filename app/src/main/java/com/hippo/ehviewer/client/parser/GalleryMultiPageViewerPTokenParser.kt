2473fec7765fed8fe044485304e7323c6bfeddad
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-04-02 14:56:00 +0800

    Convert GalleryMultiPageViewerPTokenParser to kotlin

--- app/src/main/java/com/hippo/ehviewer/client/parser/GalleryMultiPageViewerPTokenParser.kt
+++ app/src/main/java/com/hippo/ehviewer/client/parser/GalleryMultiPageViewerPTokenParser.kt
@@ -15,31 +15,24 @@
  * You should have received a copy of the GNU General Public License along with EhViewer.
  * If not, see <https://www.gnu.org/licenses/>.
  */
+package com.hippo.ehviewer.client.parser
 
-package com.hippo.ehviewer.client.parser;
+import com.hippo.ehviewer.client.exception.EhException
+import com.hippo.util.ExceptionUtils
+import org.json.JSONArray
 
-import com.hippo.ehviewer.client.exception.EhException;
-import com.hippo.util.ExceptionUtils;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-public class GalleryMultiPageViewerPTokenParser {
-    public static ArrayList<String> parse(String body) throws Exception {
-        ArrayList<String> list = new ArrayList<>();
-        String imagelist = body.substring(body.indexOf("var imagelist = ") + 16, body.indexOf(";", body.indexOf("var imagelist = ")));
-        try {
-            JSONArray ja = new JSONArray(imagelist);
-            for (int i = 0; i < ja.length(); i++) {
-                JSONObject jo = ja.getJSONObject(i);
-                list.add(jo.getString("k"));
-            }
-        } catch (Throwable e) {
-            ExceptionUtils.throwIfFatal(e);
-            throw new EhException(body);
+object GalleryMultiPageViewerPTokenParser {
+    fun parse(body: String): List<String> {
+        val imagelist = body.substring(
+            body.indexOf("var imagelist = ") + 16,
+            body.indexOf(";", body.indexOf("var imagelist = ")),
+        )
+        return runCatching {
+            val ja = JSONArray(imagelist)
+            (0 until ja.length()).map { ja.getJSONObject(it).getString("k") }
+        }.getOrElse {
+            ExceptionUtils.throwIfFatal(it)
+            throw EhException(body)
         }
-        return list;
     }
 }
