95d9f3c7a793cc91f986383de8170ad487f795c0
tarsin <43900799+asuka-mio@users.noreply.github.com>
2023-04-21 12:50:57 +0800

    Revert "Update plugin com.android.application to v8.1.0-beta01"
    This reverts commit 0a5b9e31785bad83f9a773a0e0fd7cd70a0dd508.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.1.0-beta01" apply false
+    id("com.android.application") version "8.1.0-alpha11" apply false
     kotlin("android") version "1.8.20" apply false
     kotlin("plugin.serialization") version "1.8.20" apply false
     id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
