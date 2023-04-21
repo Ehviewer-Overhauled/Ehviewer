9d76132a5de0d5c0ea0e6ab9ee5e52e35df3bf8f
tarsin <yuanqingxiang233@163.com>
2023-04-21 13:44:36 +0800

    Build with JDK 17 LTS
    Since JDK 20 is out, there is no more jdk19 on ArchLinux, but kotlin compiler don't recognise JvmTarget 20 yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,12 +86,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "19"
+        jvmTarget = "17"
         freeCompilerArgs = listOf(
             // https://kotlinlang.org/docs/compiler-reference.html#progressive
             "-progressive",
