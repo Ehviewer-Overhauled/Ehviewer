1d14e3ceb46f22bf7e109381d3262f5b6199b708
tarsin <yuanqingxiang233@163.com>
2023-05-15 12:58:53 +0800

    Fix GalleryDetailHeaderCard thumb flicker

--- app/src/main/java/com/hippo/ehviewer/ui/compose/data/GalleryDetail.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/compose/data/GalleryDetail.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -102,7 +103,7 @@ fun GalleryDetailHeaderCard(
     Row {
         Card {
             EhAsyncCropThumb(
-                model = info.thumb,
+                model = remember(info.thumb) { info.thumb },
                 modifier = Modifier.height(dimensionResource(id = R.dimen.gallery_detail_thumb_height)).width(dimensionResource(id = R.dimen.gallery_detail_thumb_width)),
             )
         }
