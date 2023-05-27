f8938a46bb0a318987ead999f648716bc962bfbd
tarsin <yuanqingxiang233@163.com>
2023-05-28 01:18:58 +0800

    Fix naming

--- app/src/main/java/com/hippo/ehviewer/client/EhDns.kt
+++ app/src/main/java/com/hippo/ehviewer/client/EhDns.kt
@@ -23,7 +23,7 @@ import java.net.UnknownHostException
 private typealias HostsMap = MutableMap<String, List<InetAddress>>
 private fun hostsDsl(builder: HostsMap.() -> Unit): HostsMap = mutableMapOf<String, List<InetAddress>>().apply(builder)
 private fun interface HostMapBuilder {
-    infix fun String.availableInCN(boolean: Boolean)
+    infix fun String.blockedInCN(boolean: Boolean)
 }
 
 private fun HostsMap.hosts(vararg hosts: String, builder: HostMapBuilder.() -> Unit) = apply {
@@ -40,49 +40,49 @@ object EhDns : Dns {
     private val builtInHosts = hostsDsl {
         // ip: String!, blockedByCCP: Boolean!
         hosts("ehgt.org", "gt0.ehgt.org", "gt1.ehgt.org", "gt2.ehgt.org", "gt3.ehgt.org") {
-            "37.48.89.44" availableInCN false
-            "81.171.10.48" availableInCN false
-            "178.162.139.24" availableInCN false
-            "178.162.140.212" availableInCN false
-            "2001:1af8:4700:a062:8::47de" availableInCN false
-            "2001:1af8:4700:a062:9::47de" availableInCN true
-            "2001:1af8:4700:a0c9:4::47de" availableInCN false
-            "2001:1af8:4700:a0c9:3::47de" availableInCN true
+            "37.48.89.44" blockedInCN false
+            "81.171.10.48" blockedInCN false
+            "178.162.139.24" blockedInCN false
+            "178.162.140.212" blockedInCN false
+            "2001:1af8:4700:a062:8::47de" blockedInCN false
+            "2001:1af8:4700:a062:9::47de" blockedInCN true
+            "2001:1af8:4700:a0c9:4::47de" blockedInCN false
+            "2001:1af8:4700:a0c9:3::47de" blockedInCN true
         }
         hosts("e-hentai.org") {
-            "104.20.134.21" availableInCN false
-            "104.20.135.21" availableInCN false
-            "172.67.0.127" availableInCN false
+            "104.20.134.21" blockedInCN false
+            "104.20.135.21" blockedInCN false
+            "172.67.0.127" blockedInCN false
         }
         hosts("exhentai.org") {
-            "178.175.128.252" availableInCN false
-            "178.175.129.252" availableInCN false
-            "178.175.129.254" availableInCN false
-            "178.175.128.254" availableInCN false
-            "178.175.132.20" availableInCN false
-            "178.175.132.22" availableInCN false
+            "178.175.128.252" blockedInCN false
+            "178.175.129.252" blockedInCN false
+            "178.175.129.254" blockedInCN false
+            "178.175.128.254" blockedInCN false
+            "178.175.132.20" blockedInCN false
+            "178.175.132.22" blockedInCN false
         }
         hosts("s.exhentai.org") {
-            "178.175.129.254" availableInCN false
-            "178.175.128.254" availableInCN false
-            "178.175.132.22" availableInCN false
+            "178.175.129.254" blockedInCN false
+            "178.175.128.254" blockedInCN false
+            "178.175.132.22" blockedInCN false
         }
         hosts("repo.e-hentai.org") {
-            "94.100.28.57" availableInCN true
-            "94.100.29.73" availableInCN true
+            "94.100.28.57" blockedInCN true
+            "94.100.29.73" blockedInCN true
         }
         hosts("forums.e-hentai.org") {
-            "94.100.18.243" availableInCN false
+            "94.100.18.243" blockedInCN false
         }
         hosts("ul.ehgt.org") {
-            "94.100.24.82" availableInCN true
-            "94.100.24.72" availableInCN true
+            "94.100.24.82" blockedInCN true
+            "94.100.24.72" blockedInCN true
         }
         hosts("raw.githubusercontent.com") {
-            "151.101.0.133" availableInCN false
-            "151.101.64.133" availableInCN false
-            "151.101.128.133" availableInCN false
-            "151.101.192.133" availableInCN false
+            "151.101.0.133" blockedInCN false
+            "151.101.64.133" blockedInCN false
+            "151.101.128.133" blockedInCN false
+            "151.101.192.133" blockedInCN false
         }
     }
 
