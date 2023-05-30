1f0319c24201269a5f7d4fd9710d847854ee7666
tarsin <yuanqingxiang233@163.com>
2023-05-30 23:49:29 +0800

    Tweak showSelectItemWithIcon style

--- app/src/main/java/com/hippo/ehviewer/ui/compose/Dialog.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/compose/Dialog.kt
@@ -4,10 +4,12 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -125,16 +128,15 @@ class DialogState {
                             color = AlertDialogDefaults.containerColor,
                             tonalElevation = AlertDialogDefaults.TonalElevation,
                         ) {
-                            Column(
-                                modifier = Modifier.padding(24.dp),
-                            ) {
-                                Text(text = title)
+                            Column {
+                                Text(text = title, modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
                                 items.forEachIndexed { index, (icon, text) ->
                                     Row(
                                         modifier = Modifier.clickable {
                                             dismiss()
                                             cont.resume(index)
-                                        },
+                                        }.fillMaxWidth(),
+                                        verticalAlignment = Alignment.CenterVertically,
                                     ) {
                                         Icon(imageVector = icon, contentDescription = null, modifier = Modifier.padding(16.dp))
                                         Text(text = stringResource(id = text))
