4d85a2aa413b71e4cbf5221187761fbc97c7d57a
tarsin <yuanqingxiang233@163.com>
2023-05-31 12:29:29 +0800

    Resolve #1120

--- app/src/main/java/com/hippo/ehviewer/network/UrlBuilder.kt
+++ app/src/main/java/com/hippo/ehviewer/network/UrlBuilder.kt
@@ -5,7 +5,7 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 class UrlBuilder(mRootUrl: String) {
     private val url = mRootUrl.toHttpUrl().newBuilder()
     fun addQuery(key: String, value: Any) {
-        url.addQueryParameter(key, value.toString())
+        url.addEncodedQueryParameter(key, value.toString())
     }
 
     fun build(): String {
