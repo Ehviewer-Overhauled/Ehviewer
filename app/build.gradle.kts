a4eca582759ceeb82526cf2c6e2d367a9e607618
tarsin <yuanqingxiang233@163.com>
2023-06-02 15:53:18 +0800

    Don't opt-in InternalCoroutinesApi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,6 @@ android {
             "-opt-in=kotlin.contracts.ExperimentalContracts",
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
             "-opt-in=kotlinx.coroutines.FlowPreview",
-            "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
             "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
             "-opt-in=splitties.experimental.ExperimentalSplittiesApi",
         )
