f39d17abc0faedd6af9127dc356f08c8cd85f7f6
tarsin <yuanqingxiang233@163.com>
2023-06-04 14:42:56 +0800

    Introduce new compose SettingsActivity stub

---
+++ app/src/main/java/com/hippo/ehviewer/ui/settings/SettingsActivity.kt
@@ -0,0 +1,34 @@
+package com.hippo.ehviewer.ui.settings
+
+import android.os.Bundle
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hippo.ehviewer.ui.EhActivity
+import com.hippo.ehviewer.ui.compose.setMD3Content
+import com.hippo.ehviewer.ui.login.LocalNavController
+
+class SettingsActivity : EhActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setMD3Content {
+            val navController = rememberNavController()
+
+            CompositionLocalProvider(LocalNavController provides navController) {
+                NavHost(navController = navController, startDestination = BASE_SETTINGS_SCREEN) {
+                    composable(BASE_SETTINGS_SCREEN) {
+                    }
+                }
+            }
+        }
+    }
+
+    override fun finish() {
+        super.finish()
+        ActivityNavigator.applyPopAnimationsToPendingTransition(this)
+    }
+}
+
+const val BASE_SETTINGS_SCREEN = "Base"
