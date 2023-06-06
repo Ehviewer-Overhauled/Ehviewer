0905a4dfba0ed970526b27e7056f41712cb41f7b
tarsin <yuanqingxiang233@163.com>
2023-06-06 23:08:42 +0800

    FilterScreen help message

--- app/src/main/java/com/hippo/ehviewer/ui/settings/FilterScreen.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/settings/FilterScreen.kt
@@ -3,19 +3,23 @@ package com.hippo.ehviewer.ui.settings
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Help
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.hippo.ehviewer.R
 import com.hippo.ehviewer.ui.LocalNavController
+import com.hippo.ehviewer.ui.legacy.BaseDialogBuilder
 
 @Composable
 fun FilterScreen() {
     val navController = LocalNavController.current
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -23,6 +27,11 @@ fun FilterScreen() {
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { BaseDialogBuilder(context).setTitle(R.string.filter).setMessage(R.string.filter_tip).setPositiveButton(android.R.string.ok, null).show() }) {
+                        Icon(imageVector = Icons.Default.Help, contentDescription = null)
                     }
                 },
             )
