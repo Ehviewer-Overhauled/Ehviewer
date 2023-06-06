c68c011e6999370d04c9d0e652af00cb3ec3f3b0
tarsin <yuanqingxiang233@163.com>
2023-06-06 23:05:51 +0800

    FilterScreen stub

--- app/src/main/java/com/hippo/ehviewer/ui/settings/FilterScreen.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/settings/FilterScreen.kt
@@ -1,7 +1,34 @@
 package com.hippo.ehviewer.ui.settings
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.hippo.ehviewer.R
+import com.hippo.ehviewer.ui.LocalNavController
 
 @Composable
 fun FilterScreen() {
+    val navController = LocalNavController.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.filter)) },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        LazyColumn(contentPadding = paddingValues) {
+        }
+    }
 }
