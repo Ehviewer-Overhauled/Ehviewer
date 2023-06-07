e149c7065eb691e06a8879097bf350cf1f67c8ed
tarsin <yuanqingxiang233@163.com>
2023-06-07 15:46:50 +0800

    Upgrade r8

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,6 @@ tasks.register("Delete", Delete::class) {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools:r8:8.2.9-dev")
+        classpath("com.android.tools:r8:8.2.10-dev")
     }
 }
