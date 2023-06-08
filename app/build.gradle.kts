29fafdf216e297de2e9f7b98993346e672e50f5a
tarsin <yuanqingxiang233@163.com>
2023-06-08 16:53:32 +0800

    Upgrade paging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -192,8 +192,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
     // https://developer.android.com/jetpack/androidx/releases/paging
-    implementation("androidx.paging:paging-common-ktx:3.2.0-beta01")
-    implementation("androidx.paging:paging-compose:1.0.0-alpha20")
+    implementation("androidx.paging:paging-compose:3.2.0-beta01")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
 
     // https://developer.android.com/jetpack/androidx/releases/room
