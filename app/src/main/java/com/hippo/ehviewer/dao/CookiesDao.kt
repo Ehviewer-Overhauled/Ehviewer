2fbda42e530f516b7548183427a7fbccaa1bb04b
tarsin <yuanqingxiang233@163.com>
2023-06-08 16:26:49 +0800

    Cleanup CookiesDao

--- app/src/main/java/com/hippo/ehviewer/dao/CookiesDao.kt
+++ app/src/main/java/com/hippo/ehviewer/dao/CookiesDao.kt
@@ -1,22 +1,10 @@
 package com.hippo.ehviewer.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 
 @Dao
 interface CookiesDao {
     @Query("SELECT * FROM OK_HTTP_3_COOKIE")
     fun list(): List<Cookie>
-
-    @Delete
-    fun delete(cookie: Cookie)
-
-    @Insert
-    fun insert(cookie: Cookie): Long
-
-    @Update
-    fun update(cookie: Cookie)
 }
