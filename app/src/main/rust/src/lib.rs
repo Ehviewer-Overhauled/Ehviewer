9e4fc2e7af7014b820c39fd4ba9382226ac32b32
tarsin <yuanqingxiang233@163.com>
2023-06-16 00:16:58 +0800

    Spilt torrent.rs

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,3 +1,5 @@
+mod parser;
+
 extern crate android_logger;
 extern crate apply;
 extern crate catch_panic;
@@ -24,6 +26,7 @@ use quick_xml::escape::unescape;
 use std::ffi::c_void;
 use tl::{Node, Parser, VDom};
 
+#[macro_export]
 macro_rules! regex {
     ($re:literal $(,)?) => {{
         static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
@@ -43,18 +46,6 @@ impl<'a> Anon for VDom<'a> {
         let handle = self.get_elements_by_class_name(name).next()?;
         Some(handle.get(self.parser())?)
     }
-}
-
-#[derive(Default, IntoJava)]
-#[jnix(package = "com.hippo.ehviewer.client.parser")]
-pub struct Torrent {
-    posted: String,
-    size: String,
-    seeds: i32,
-    peers: i32,
-    downloads: i32,
-    url: String,
-    name: String,
 }
 
 #[derive(Default, IntoJava)]
@@ -82,12 +73,6 @@ pub struct BaseGalleryInfo {
     simpleLanguage: String,
     favoriteSlot: i32,
     favoriteName: Option<String>,
-}
-
-#[derive(Default, IntoJava)]
-#[jnix(package = "com.hippo.ehviewer.client.parser")]
-pub struct TorrentResult {
-    list: Vec<Torrent>,
 }
 
 #[derive(Default, IntoJava)]
@@ -173,33 +158,6 @@ pub fn parseFav(env: JNIEnv, _class: JClass, input: JString, str: jobjectArray) 
     env.new_int_array(10)
         .unwrap()
         .also(|it| env.set_int_array_region(*it, 0, &vec).unwrap())
-}
-
-#[no_mangle]
-#[catch_panic(default = "std::ptr::null_mut()")]
-#[allow(non_snake_case)]
-#[jni_fn("com.hippo.ehviewer.client.parser.TorrentParserKt")]
-pub fn parseTorrent(env: JNIEnv, _class: JClass, input: JString) -> jobject {
-    let mut env = JnixEnv { env };
-    parse_jni_string(&mut env, &input, |dom, parser, _env| {
-        Some(TorrentResult {
-            list: dom.query_selector("table")?.filter_map(|e| {
-                let html = e.get(parser)?.inner_html(parser);
-                let reg = regex!("</span> ([0-9-]+) [0-9:]+</td>[\\s\\S]+</span> ([0-9.]+ [KMGT]B)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span>([^<]+)</td>[\\s\\S]+onclick=\"document.location='([^\"]+)'[^<]+>([^<]+)</a>");
-                let grp = reg.captures(&html)?;
-                let name = unescape(&grp[8]).ok()?;
-                Some(Torrent {
-                    posted: grp[1].to_string(),
-                    size: grp[2].to_string(),
-                    seeds: grp[3].parse().ok()?,
-                    peers: grp[4].parse().ok()?,
-                    downloads: grp[5].parse().ok()?,
-                    url: grp[7].to_string(),
-                    name: name.to_string()
-                })
-            }).collect()
-        })
-    }).unwrap().into_java(&env).forget().into_raw()
 }
 
 #[no_mangle]
---
+++ app/src/main/rust/src/parser/torrent.rs
@@ -0,0 +1,53 @@
+use catch_panic::catch_panic;
+use jni_fn::jni_fn;
+use jnix::jni::objects::{JClass, JString};
+use jnix::jni::sys::jobject;
+use jnix::jni::JNIEnv;
+use jnix::{IntoJava, JnixEnv};
+use jnix_macros::IntoJava;
+use quick_xml::escape::unescape;
+use {parse_jni_string, regex};
+
+#[derive(Default, IntoJava)]
+#[jnix(package = "com.hippo.ehviewer.client.parser")]
+pub struct Torrent {
+    posted: String,
+    size: String,
+    seeds: i32,
+    peers: i32,
+    downloads: i32,
+    url: String,
+    name: String,
+}
+
+#[derive(Default, IntoJava)]
+#[jnix(package = "com.hippo.ehviewer.client.parser")]
+pub struct TorrentResult {
+    list: Vec<Torrent>,
+}
+#[no_mangle]
+#[catch_panic(default = "std::ptr::null_mut()")]
+#[allow(non_snake_case)]
+#[jni_fn("com.hippo.ehviewer.client.parser.TorrentParserKt")]
+pub fn parseTorrent(env: JNIEnv, _class: JClass, input: JString) -> jobject {
+    let mut env = JnixEnv { env };
+    parse_jni_string(&mut env, &input, |dom, parser, _env| {
+        Some(TorrentResult {
+            list: dom.query_selector("table")?.filter_map(|e| {
+                let html = e.get(parser)?.inner_html(parser);
+                let reg = regex!("</span> ([0-9-]+) [0-9:]+</td>[\\s\\S]+</span> ([0-9.]+ [KMGT]B)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span> ([0-9]+)</td>[\\s\\S]+</span>([^<]+)</td>[\\s\\S]+onclick=\"document.location='([^\"]+)'[^<]+>([^<]+)</a>");
+                let grp = reg.captures(&html)?;
+                let name = unescape(&grp[8]).ok()?;
+                Some(Torrent {
+                    posted: grp[1].to_string(),
+                    size: grp[2].to_string(),
+                    seeds: grp[3].parse().ok()?,
+                    peers: grp[4].parse().ok()?,
+                    downloads: grp[5].parse().ok()?,
+                    url: grp[7].to_string(),
+                    name: name.to_string()
+                })
+            }).collect()
+        })
+    }).unwrap().into_java(&env).forget().into_raw()
+}
