b0346b4e9df15d3d1925b98d24cffe0f920fbd3b
tarsin <43900799+asuka-mio@users.noreply.github.com>
2023-07-01 10:30:09 +0800

    Revert guava

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -250,7 +250,7 @@ dependencies {
     implementation("io.ktor:ktor-utils-jvm:$ktor")
 
     implementation("org.chromium.net:cronet-embedded:113.5672.61")
-    implementation("com.google.guava:guava:32.1.1-android")
+    implementation("com.google.guava:guava:32.0.1-android")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.5.1")
