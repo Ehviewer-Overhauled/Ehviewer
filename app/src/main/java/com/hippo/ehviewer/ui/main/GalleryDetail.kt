972bf973919ffdf48687aa5afbd5dd1bd1299681
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-07-01 19:14:18 +0800

    Move error tip below header in gallery detail

--- app/src/main/java/com/hippo/ehviewer/ui/main/GalleryDetail.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/main/GalleryDetail.kt
@@ -1,12 +1,16 @@
 package com.hippo.ehviewer.ui.main
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.GroupOff
 import androidx.compose.material.icons.filled.Label
@@ -22,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -146,4 +151,22 @@ fun GalleryDetailHeaderCard(
             )
         }
     }
+}
+
+@Composable
+fun GalleryDetailErrorTip(error: String, onClick: () -> Unit) = Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.big_sad_pandroid),
+        contentDescription = null,
+        modifier = Modifier.clickable(onClick = onClick),
+    )
+    Spacer(modifier = Modifier.size(8.dp))
+    Text(
+        text = error,
+        modifier = Modifier.widthIn(max = 228.dp),
+    )
 }
