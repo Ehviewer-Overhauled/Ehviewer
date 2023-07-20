f51e6603665ba13e18e633c589cad45ecbd885a2
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-07-20 23:22:06 +0800

    Upgrade db, add unique index for download label

--- app/src/main/java/com/hippo/ehviewer/dao/DownloadLabel.kt
+++ app/src/main/java/com/hippo/ehviewer/dao/DownloadLabel.kt
@@ -2,9 +2,10 @@ package com.hippo.ehviewer.dao
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "DOWNLOAD_LABELS")
+@Entity(tableName = "DOWNLOAD_LABELS", indices = [Index("LABEL", unique = true)])
 data class DownloadLabel(
     @ColumnInfo(name = "LABEL")
     var label: String,
