0a5600d94986229c510922acb9e32368b90a6d73
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-07-28 14:47:27 +0800

    Cleanup code

--- app/src/main/java/com/hippo/ehviewer/spider/SpiderQueen.kt
+++ app/src/main/java/com/hippo/ehviewer/spider/SpiderQueen.kt
@@ -55,7 +55,6 @@ import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.sync.withPermit
 import moe.tarsin.coroutines.runSuspendCatching
 import splitties.init.appCtx
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
 class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineScope {
@@ -66,11 +65,9 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
     lateinit var mSpiderInfo: SpiderInfo
 
     val mSpiderDen: SpiderDen = SpiderDen(galleryInfo)
-    val mPagePercentMap = ConcurrentHashMap<Int, Float>()
     private val mPageStateLock = Any()
     private val mDownloadedPages = AtomicInteger(0)
     private val mFinishedPages = AtomicInteger(0)
-    private val mPageErrorMap = ConcurrentHashMap<Int, String>()
     private val mSpiderListeners: MutableList<OnSpiderListener> = ArrayList()
 
     private var mReadReference = 0
@@ -197,8 +194,6 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
                     }
                     mDownloadedPages.lazySet(0)
                     mFinishedPages.lazySet(0)
-                    mPageErrorMap.clear()
-                    mPagePercentMap.clear()
                 }
                 mWorkerScope.enterDownloadMode()
             }
@@ -442,7 +437,7 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
 
     fun updatePageState(index: Int, @State state: Int, error: String? = null) {
         var oldState: Int
-        synchronized(mPageStateLock) {
+        synchronized<Unit>(mPageStateLock) {
             oldState = mPageStateArray[index]
             mPageStateArray[index] = state
             if (!isStateDone(oldState) && isStateDone(state)) {
@@ -454,18 +449,6 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
                 mFinishedPages.incrementAndGet()
             } else if (oldState == STATE_FINISHED && state != STATE_FINISHED) {
                 mFinishedPages.decrementAndGet()
-            }
-
-            // Clear
-            if (state == STATE_DOWNLOADING) {
-                mPageErrorMap.remove(index)
-            } else if (isStateDone(state)) {
-                mPagePercentMap.remove(index)
-            }
-
-            // Get default error
-            if (state == STATE_FAILED) {
-                mPageErrorMap[index] = error ?: appCtx.getString(R.string.error_unknown)
             }
         }
 
@@ -748,7 +731,6 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
                         targetImageUrl,
                         referer,
                     ) { contentLength: Long, receivedSize: Long, bytesRead: Int ->
-                        mPagePercentMap[index] = receivedSize.toFloat() / contentLength
                         notifyPageDownload(index, contentLength, receivedSize, bytesRead)
                     }
 
