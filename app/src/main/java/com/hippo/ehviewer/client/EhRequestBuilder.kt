db57e2dde6033d50361d8111f0480f8eb2019e00
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-08-05 17:53:01 +0800

    Use mobile UA for Cloudflare bypass
    Allow custom UA in the future?

--- app/src/main/java/com/hippo/ehviewer/client/EhRequestBuilder.kt
+++ app/src/main/java/com/hippo/ehviewer/client/EhRequestBuilder.kt
@@ -123,6 +123,6 @@ inline fun <reified T> String.parseAs(): T = json.decodeFromString(this)
 
 val json = Json { ignoreUnknownKeys = true }
 
-const val CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+const val CHROME_USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"
 const val CHROME_ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 const val CHROME_ACCEPT_LANGUAGE = "en-US,en;q=0.5"
