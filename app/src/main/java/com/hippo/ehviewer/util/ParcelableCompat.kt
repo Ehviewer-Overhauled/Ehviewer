7af30dc3221cc600a2cb501739ad75cd6046332f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-08-08 21:05:44 +0800

    Revert "Workaround #1065"
    This reverts commit 576ff1b0
    
    Reason for revert: Fixed in `androidx.core:core:1.12.0-beta01`.
    https://android.googlesource.com/platform/frameworks/support/+/8f7e02550620919fafb4c7fef8743f8c20562945

--- app/src/main/java/com/hippo/ehviewer/util/ParcelableCompat.kt
+++ app/src/main/java/com/hippo/ehviewer/util/ParcelableCompat.kt
@@ -1,18 +1,11 @@
-@file:Suppress("DEPRECATION")
-
 package com.hippo.ehviewer.util
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.content.IntentCompat
+import androidx.core.os.BundleCompat
 
-// inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String?): T? = BundleCompat.getParcelable(this, key, T::class.java)
-// inline fun <reified T : Parcelable> Bundle.getParcelableArrayListCompat(key: String?): ArrayList<T>? = BundleCompat.getParcelableArrayList(this, key, T::class.java)
-// inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String?): T? = IntentCompat.getParcelableExtra(this, key, T::class.java)
-
-// Avoid using new API on Tiramisu
-// See https://issuetracker.google.com/issues/240585930 & https://github.com/Ehviewer-Overhauled/Ehviewer/issues/1065
-
-inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String?): T? = getParcelable(key)
-inline fun <reified T : Parcelable> Bundle.getParcelableArrayListCompat(key: String?): ArrayList<T>? = getParcelableArrayList(key)
-inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String?): T? = getParcelableExtra(key)
+inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String?): T? = BundleCompat.getParcelable(this, key, T::class.java)
+inline fun <reified T : Parcelable> Bundle.getParcelableArrayListCompat(key: String?): ArrayList<T>? = BundleCompat.getParcelableArrayList(this, key, T::class.java)
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String?): T? = IntentCompat.getParcelableExtra(this, key, T::class.java)
