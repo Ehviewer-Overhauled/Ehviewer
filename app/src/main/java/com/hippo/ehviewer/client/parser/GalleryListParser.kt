0a665a830c298841035e9615878f34bc2c380c0b
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-08-10 16:48:05 +0800

    Fix error message when no hits found

--- app/src/main/java/com/hippo/ehviewer/client/parser/GalleryListParser.kt
+++ app/src/main/java/com/hippo/ehviewer/client/parser/GalleryListParser.kt
@@ -6,20 +6,22 @@ import com.hippo.ehviewer.client.data.GalleryInfo.Companion.LOCAL_FAVORITED
 import com.hippo.ehviewer.client.data.GalleryInfo.Companion.NOT_FAVORITED
 
 object GalleryListParser {
-    suspend fun parse(body: String) = parseGalleryInfoList(body).apply {
-        galleryInfoList.onEach {
-            if (it.favoriteSlot == NOT_FAVORITED && EhDB.containLocalFavorites(it.gid)) {
-                it.favoriteSlot = LOCAL_FAVORITED
+    suspend fun parse(body: String) = runCatching {
+        parseGalleryInfoList(body).apply {
+            galleryInfoList.onEach {
+                if (it.favoriteSlot == NOT_FAVORITED && EhDB.containLocalFavorites(it.gid)) {
+                    it.favoriteSlot = LOCAL_FAVORITED
+                }
+                it.generateSLang()
             }
-            it.generateSLang()
         }
-    }
+    }.getOrDefault(GalleryListResult())
 }
 
 class GalleryListResult(
-    val prev: String?,
-    val next: String?,
-    val galleryInfoList: ArrayList<BaseGalleryInfo>,
+    val prev: String? = null,
+    val next: String? = null,
+    val galleryInfoList: ArrayList<BaseGalleryInfo> = arrayListOf(),
 )
 
 private external fun parseGalleryInfoList(e: String): GalleryListResult
